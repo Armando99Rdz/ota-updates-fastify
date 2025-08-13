b90ab4f3f29326268253028118448f5b441ad2a5
Armando <armando_rodriguez@toditopagos.com>
2025-08-13 13:07:07 -0600

    publish.sh arg option (new version/rollback)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,18 +9,28 @@ GRAY='\033[2;37m'
 GREEN='\033[32m'
 YELLOW='\033[33m'
 
+
+# -----------------------------------
+ARG_ROLLBACK="r"
+ARG_NEW_VERSION="p"
+
+while getopts d: flag
+do
+    case "${flag}" in
+        d) ARG=${OPTARG};;
+    esac
+done
+
 # -----------------------------------
 echo "${BOLD}APP PATH:${RESET} $APP_PATH"
-echo "${BOLD}SERVER PATH:${RESET} $SERVER_PATH"
+echo "${BOLD}SERVER PATH:${RESET} $SERVER_PATH ${RESET}"
+if [[ "${ARG:-}" == "$ARG_NEW_VERSION" ]]; then
+    echo "${YELLOW}Rollback Version ${RESET}"
+elif [[ "${ARG:-}" == "$ARG_ROLLBACK" ]]; then
+    echo "New Version ${RESET}"
+fi
 echo ""
 
-# bash args
-# while getopts d: flag
-# do
-#     case "${flag}" in
-#         d) directory=${OPTARG};;
-#     esac
-# done
 
 # -----------------------------------
 # user input
@@ -28,12 +38,17 @@ echo "> Enter the version ID: ${LIGHT}${GRAY}1.0.0(1), 1755037367, 100, etc.${RE
 read versionId
 echo "${RESET}"
 
-updateDir="updates/$versionId/$(date +%s)"
+if [[ "${ARG:-}" == "$ARG_NEW_VERSION" ]]; then
+    updateDir="updates/$versionId/$(date +%s)"
+elif [[ "${ARG:-}" == "$ARG_ROLLBACK" ]]; then
+    updateDir="updates/$versionId/$(date +%s)/rollback"
+fi
 
 if [[ -d "$updateDir" ]]; then
     echo "⚠️  ${YELLOW}Al parecer ya existe una version con este ID. (${updateDir}) ${RESET}"
     exit 1
 fi
+
 
 # Prepare app expo version
 cd $APP_PATH
@@ -46,4 +61,14 @@ mkdir -p "$updateDir"
 cp -r $APP_PATH/dist/ $updateDir
 
 node ./scripts/exportClientExpoConfig.js $APP_PATH $SERVER_PATH > $updateDir/expoConfig.json
-echo "Done!"
+
+if [[ "${ARG:-}" == "$ARG_NEW_VERSION" ]]; then
+    
+    echo "${GREEN}${BOLD}DONE ${RESET}${BOLD}New version published"
+    echo ""
+    
+elif [[ "${ARG:-}" == "$ARG_ROLLBACK" ]]; then
+
+    echo "${GREEN}${BOLD}DONE ${RESET}${BOLD}Rollback version published"
+    echo ""
+fi
--- scripts/rollback.sh
+++
@@ -1,5 +0,0 @@
-# by @ArmandoRdz
-
-
-# original command:
-# mktouch() { mkdir -p $(dirname $1) && touch $1; }; cd ../api-ota-updates && mktouch updates/1/$(date +%s)/rollback
